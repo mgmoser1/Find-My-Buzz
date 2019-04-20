034467ab4b6f4d303a8a21bcf5d6d0d7ff130ab7
MGM <mgmoser1@users.noreply.github.com>
2019-04-20 17:53:45 -0500

    added gitignore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(barFinder){
 
-
+// New YELP API Key: here's the new key JARq9NBksYNIfR1HQQ8z3P5r7ypZW9-Xo_bVQUO-QRgXM3XJbnpvhKuo25EXjDrm1Xq8A9Vv6-p9dHcRJlH6dVqQVbXLU_iq3CYqI1YVwxyD12qLi0-xDNo8_ba5XHYx
 
   // Initialize Firebase
   var config = {
@@ -12,6 +12,37 @@ $(document).ready(function(barFinder){
       messagingSenderId: "1035338020280"
     };
     firebase.initializeApp(config);
-        
+  
+
+    // GLOBAL VARIABLES //
+
+    // this is the variable to use when pushing to / pulling from firebase
+    var barDB = firebase.database();
+
+    var comments = "";
+    var commentorName = "";
+
+
+  // BAR BUTTON ON-CLICK //
+  $(document).on("click",".bar-code", function (barSubmit) {
+    event.preventDefault();
+
+    var businessID = $(this).attr("data-point");
+    // Path for the second search by Bar ID. //
+
+    var existingComments = [];
+    var commentsPresent = checkDB(businessID);
+    function checkDB(n) {
+      if (n)
+    }
+
+    var buzzDetailURL =
+    'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/' + businessID;
+  
+  // Path for the third search by Bar ID for reviews. //
+    var buzzReviewURL =
+    'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/' + businessID + '/reviews';
+  
+     
     
 });
